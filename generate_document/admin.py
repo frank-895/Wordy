9e24a2830f617f58d9a73e1d7b1514b860b69add
Frank <frank.snelling03@gmail.com>
2025-06-21 14:51:44 +0100

    Update admin.py

--- generate_document/admin.py
+++ generate_document/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Template
 
 # Register your models here.
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
